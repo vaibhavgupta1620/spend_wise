4e4245bb390c329535c76048e1a9bbfaa9621add
Vaibhav Gupta <vaibhavgupta1620@gmail.com>
2025-12-25 00:01:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -113,10 +113,10 @@ async function checkSrvIfNeeded(uri) {
 ------------------------------------------------------------------ */
 
 async function connectMongo() {
-  const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/spendwise';
+  const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/spendwise';
 
   if (!uri) {
-    throw new Error('MONGO_URI not defined');
+    throw new Error('MONGODB_URI not defined');
   }
 
   if (uri.startsWith('mongodb+srv://')) {
